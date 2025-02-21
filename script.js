472d68af54ec8c04d6f08b6d6e8e4fd079cea5c4
iM Mustafa <im262897tt@gmail.com>
2025-02-20 21:33:39 -0800

    start javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+// Select DOM elements
+const noteInput = document.getElementById('noteInput');
+const addNoteBtn = document.getElementById('addNote');
+const notesDisplay = document.getElementById('notesDisplay');
+const clearNotesBtn = document.getElementById('clearNotes');
